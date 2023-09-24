5d51b2fb011ba1d630e642cc98b6cecec7921a6c
JeJutic <kinga8171@gmail.com>
2023-09-24 19:06:55 +0500

    added getters to all fields with @Value

--- src/main/java/pan/artem/tinkoff/Weather.java
+++ src/main/java/pan/artem/tinkoff/Weather.java
@@ -1,35 +1,20 @@
 package pan.artem.tinkoff;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Value;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@Value
+@AllArgsConstructor
 public class Weather {
-    @Getter
-    private final UUID id;
-    private final String region;
-    @Getter
-    private final int temperature;
-    private final Instant dateTime;
-
-    public Weather(UUID id, String region, int temperature, Instant dateTime) {
-        this.id = id;
-        this.region = region;
-        this.temperature = temperature;
-        this.dateTime = dateTime;
-    }
+    UUID id;
+    String region;
+    int temperature;
+    Instant dateTime;
 
     public Weather(String region, int temperature) {
         this(UUID.randomUUID(), region, temperature, Instant.now());
-    }
-
-    @Override
-    public String toString() {
-        return "Weather{" +
-                "region='" + region + '\'' +
-                ", temperature=" + temperature +
-                ", dateTime=" + dateTime +
-                '}';
     }
 }
