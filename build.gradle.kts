bb6e9e44283feb21e7743c417f59155b0343db2b
JeJutic <kinga8171@gmail.com>
2023-09-24 17:56:35 +0500

    removed unused junit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    implementation("com.google.guava:guava:32.1.1-jre")
-}
-
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
@@ -23,8 +15,4 @@ java {
 
 application {
     mainClass.set("pan.artem.tinkoff.App")
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
 }
