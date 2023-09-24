a4ba1b6c428938436977ede5ee4d2a25efe0b1a6
JeJutic <kinga8171@gmail.com>
2023-09-24 18:18:38 +0500

    added fatJar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.jvm.tasks.Jar
+
 plugins {
     application
     id("io.freefair.lombok") version "8.3"
@@ -10,6 +12,23 @@ repositories {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    archiveBaseName = "${project.name}-fat"
+    manifest {
+        attributes["Main-Class"] = "pan.artem.tinkoff.App"
+    }
+    from(configurations.runtimeClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
     }
 }
 
