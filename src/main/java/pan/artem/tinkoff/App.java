b1df591c4638b3cbb5ad4a24897edc69e71aae87
JeJutic <kinga8171@gmail.com>
2023-09-26 23:08:17 +0500

    fixed

--- src/main/java/pan/artem/tinkoff/App.java
+++ src/main/java/pan/artem/tinkoff/App.java
@@ -1,42 +1,10 @@
 package pan.artem.tinkoff;
 
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
+
+import static pan.artem.tinkoff.WeatherUtils.*;
 
 public class App {
-
-    public static double averageTemperature(List<Weather> weathers) {
-        if (weathers.isEmpty()) {
-            return 0;
-        }
-        return weathers.stream()
-                .map(weather -> (double) weather.getTemperature() / weathers.size())
-                .reduce(Double::sum)
-                .get();
-    }
-
-    public static List<Weather> findHotRegions(List<Weather> weathers, int temperature) {
-        return weathers.stream()
-                .filter(weather -> weather.getTemperature() >= temperature)
-                .toList();
-    }
-
-    public static Map<UUID, Integer> listToIdMap(List<Weather> weathers) {
-        return weathers.stream()
-                .collect(Collectors.toMap(
-                        Weather::getId,
-                        Weather::getTemperature
-                ));
-    }
-
-    public static Map<Integer, List<Weather>> listToTemperatureMap(List<Weather> weathers) {
-        return weathers.stream()
-                .collect(Collectors.groupingBy(
-                        Weather::getTemperature
-                ));
-    }
 
     public static void main(String[] args) {
         List<Weather> weathers = List.of(
